e0b48fbd4f1238e4869733d95926d2bfff4e227d
G-catmint <114072401+G-catmint@users.noreply.github.com>
2022-11-27 19:47:17 +0800

    Add files via upload

---
+++ 指针详解1/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[] = { 2,3,4,5,6 };
+	int arr3[] = { 3,4,5,6,7 };
+	int* parr[] = { arr1,arr2,arr3 };
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", *(parr[i] + j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
