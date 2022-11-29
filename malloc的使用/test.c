9f01e10e8c47657fabb5b5741ff6907d7b6f6570
G-catmint <114072401+G-catmint@users.noreply.github.com>
2022-11-29 19:36:09 +0800

    Add files via upload

---
+++ malloc的使用/test.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//int main()
+//	{
+//	int*p = (int*)malloc(40);
+//	if (p == NULL)
+//	{  //有要的一步 malloc的返回值一定要检查
+//		printf("%s\n", strerror(errno));
+//	}
+//	else 
+//	{
+//		int i;
+//		for (i = 0; i <10; i++)
+//		{	
+//			printf("%d ", * (p + i) = i);
+//		}
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//calloc
+
+//int main()
+//{
+//	int* p = (int*)calloc(10, sizeof(int));
+//	if (NULL != p)
+//	{
+//		//调试发现 刚刚开辟的一块动态内存空间全是0
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* p = (int*)malloc(5 * sizeof(int));
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//
+//	}
+//	else
+//	{
+//		int i;
+//		for (i = 0; i < 10; i++)
+//		{  // ??????????????????????????????????????????????
+//			//????????????????????????????????????????????
+//			//我不是越界访问了吗 为嘛还能运行啊
+//			printf("%d ", *(p + i) = i);
+//		}
+//	}
+//	return 0;
+//}
+
+//realloc
+int main()
+{
+
+	return 0;
+}
